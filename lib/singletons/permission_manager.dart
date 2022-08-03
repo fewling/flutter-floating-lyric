5d71ed19a060b5cac41ae100f4aa20016493f76b
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-08-03 18:44:45 +0800

    added notification testing

--- lib/singletons/permission_manager.dart
+++ lib/singletons/permission_manager.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -8,20 +11,51 @@ class PermissionManager extends GetxController {
   PermissionManager._();
 
   Future<void> init() async {
+    await AwesomeNotifications().initialize(
+        null,
+        [
+          NotificationChannel(
+              channelGroupKey: 'basic_channel_group',
+              channelKey: 'basic_channel',
+              channelName: 'Basic notifications',
+              channelDescription: 'Notification channel for basic tests',
+              defaultColor: Colors.deepPurple,
+              ledColor: Colors.white)
+        ],
+        // Channel groups are only visual and are not required
+        channelGroups: [
+          NotificationChannelGroup(
+              channelGroupkey: 'basic_channel_group',
+              channelGroupName: 'Basic group')
+        ],
+        debug: true);
+
+    AwesomeNotifications().actionStream.listen((receivedNotification) =>
+        log('Received notification: ${receivedNotification.title}'));
+
+    _isNotificationGranted.value =
+        await AwesomeNotifications().isNotificationAllowed();
+
     _isSystemAlertWindowGranted.value =
         await Permission.systemAlertWindow.isGranted;
   }
 
   // reactive properties
   final _isSystemAlertWindowGranted = false.obs;
+  final _isNotificationGranted = false.obs;
 
   // getters
   bool get isSystemAlertWindowGranted => _isSystemAlertWindowGranted.value;
+  bool get isNotificationGranted => _isNotificationGranted.value;
 
   // methods:
   void requestSystemAlertWindowPermission() {
     Permission.systemAlertWindow
         .request()
         .then((value) => _isSystemAlertWindowGranted.value = value.isGranted);
+  }
+
+  void requestNotificationPermission() {
+    AwesomeNotifications().requestPermissionToSendNotifications();
   }
 }
--- lib/ui/permission_page.dart
+++ lib/ui/permission_page.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:floating_lyric/singletons/permission_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
@@ -32,6 +33,34 @@ class _PermissionPageState extends State<PermissionPage> {
                     : Icons.unpublished_outlined),
                 label: const Text('System Alert Window'),
               ),
+            ),
+            const SizedBox(height: 16),
+            Obx(
+              () => ElevatedButton.icon(
+                onPressed: _manager.isNotificationGranted
+                    ? null
+                    : () => _manager.requestNotificationPermission(),
+                icon: Icon(_manager.isNotificationGranted
+                    ? Icons.check
+                    : Icons.unpublished_outlined),
+                label: const Text('Notification'),
+              ),
+            ),
+
+            /// For testing notifications:
+            ElevatedButton(
+              onPressed: () {
+                AwesomeNotifications().createNotification(
+                  content: NotificationContent(
+                    id: 0,
+                    channelKey: 'basic_channel',
+                    body: 'This is a test notification',
+                    title: 'Test notification',
+                    color: Colors.deepPurple,
+                  ),
+                );
+              },
+              child: const Text('test'),
             ),
           ],
         ),
