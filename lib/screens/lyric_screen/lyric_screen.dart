5c28858289da3bfbd571286976c412c690b3e38f
Fewling <felixwong875@gmail.com>
2023-05-25 23:44:57 +0800

    Code refactored

--- lib/screens/lyric_screen/lyric_screen.dart
+++ lib/screens/lyric_screen/lyric_screen.dart
@@ -18,7 +18,6 @@ class LyricScreen extends ConsumerWidget {
     final progress = state.position / state.duration;
 
     final colorScheme = Theme.of(context).colorScheme;
-    final pref = ref.watch(preferenceProvider);
 
     return Stepper(
       currentStep: state.currentStep,
@@ -79,36 +78,7 @@ class LyricScreen extends ConsumerWidget {
               ? StepState.complete
               : StepState.indexed,
           title: const ListTile(title: Text('Floating Window Settings')),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                enabled: state.isFloatingWindowShown,
-                leading: Icon(state.isFloatingWindowShown
-                    ? Icons.visibility
-                    : Icons.visibility_off),
-                title: const Text('Enable'),
-                trailing: Switch(
-                  value: state.isFloatingWindowShown,
-                  onChanged: notifier.toggleWindowVisibility,
-                ),
-              ),
-              ListTile(
-                enabled: state.isFloatingWindowShown,
-                leading: const Icon(Icons.window),
-                title: const Text('Window Transparency'),
-              ),
-              Slider(
-                max: 100,
-                divisions: 20,
-                value: pref.opacity,
-                label: '${pref.opacity}%',
-                onChanged: !state.isFloatingWindowShown
-                    ? null
-                    : notifier.updateWindowOpacity,
-              ),
-            ],
-          ),
+          content: const _WindowSettingsStep(),
         ),
         Step(
           isActive: state.currentStep == 2,
@@ -116,96 +86,157 @@ class LyricScreen extends ConsumerWidget {
             title: Text('Import .lrc Files'),
             subtitle: Text('you only need to do this once'),
           ),
-          content: Column(
-            children: [
-              const ListTile(
-                  title: Text(
-                      'Pick a .LRC file matching ONE of the following conditions:')),
-              RadioListTile(
-                value: 0,
-                groupValue: ref
-                    .watch(
-                        lyricExistsProvider('${state.title} - ${state.artist}'))
-                    .when(
-                      data: (data) => data ? 0 : -1,
-                      error: (_, __) => -1,
-                      loading: () => -1,
-                    ),
-                onChanged: null,
-                title: const Text('File name:'),
-                subtitle: Text('${state.title} - ${state.artist}.lrc'),
-              ),
-              RadioListTile(
-                value: 1,
-                groupValue: ref
-                    .watch(
-                        lyricExistsProvider('${state.artist} - ${state.title}'))
-                    .when(
-                      data: (data) => data ? 1 : -1,
-                      error: (_, __) => -1,
-                      loading: () => -1,
-                    ),
-                title: const Text('File name:'),
-                subtitle: Text('${state.artist} - ${state.title}.lrc'),
-                onChanged: null,
-              ),
-              RadioListTile(
-                value: 2,
-                groupValue: -1,
-                title: Text('Your .lrc file contains:\n'
-                    '[ti:${state.title}]\n'
-                    '[ar:${state.artist}]'),
-                onChanged: null,
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                width: double.maxFinite,
-                child: FloatingActionButton.large(
-                  onPressed: () => state.isProcessingFiles
-                      ? null
-                      : notifier.pickLyrics().then((failedFiles) {
-                          ref.invalidate(allRawLyricsProvider);
-                          if (failedFiles.isNotEmpty) {
-                            showDialog(
-                              context: context,
-                              builder: (_) => FailedImportDialog(failedFiles),
-                            );
-                          }
-                        }),
-                  child: state.isProcessingFiles
-                      ? const Center(child: CircularProgressIndicator())
-                      : const Text('Import'),
-                ),
-              ),
-            ],
-          ),
+          content: const _LrcFormatStep(),
         ),
         Step(
           isActive: state.currentStep == 3,
           title: const ListTile(title: Text('Reminders')),
-          content: Column(
-            children: const [
-              ListTile(
-                title: Text(
-                  '1. You can drag the floating window to anywhere on the screen.',
-                ),
+          content: const _ReminderStep(),
+        ),
+      ],
+    );
+  }
+}
+
+class _WindowSettingsStep extends ConsumerWidget {
+  const _WindowSettingsStep();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(lyricStateProvider);
+    final notifier = ref.watch(lyricStateProvider.notifier);
+    final pref = ref.watch(preferenceProvider);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+          enabled: state.isFloatingWindowShown,
+          leading: Icon(state.isFloatingWindowShown
+              ? Icons.visibility
+              : Icons.visibility_off),
+          title: const Text('Enable'),
+          trailing: Switch(
+            value: state.isFloatingWindowShown,
+            onChanged: notifier.toggleWindowVisibility,
+          ),
+        ),
+        ListTile(
+          enabled: state.isFloatingWindowShown,
+          leading: const Icon(Icons.window),
+          title: const Text('Window Transparency'),
+        ),
+        Slider(
+          max: 100,
+          divisions: 20,
+          value: pref.opacity,
+          label: '${pref.opacity}%',
+          onChanged: !state.isFloatingWindowShown
+              ? null
+              : notifier.updateWindowOpacity,
+        ),
+      ],
+    );
+  }
+}
+
+class _LrcFormatStep extends ConsumerWidget {
+  const _LrcFormatStep();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(lyricStateProvider);
+    final notifier = ref.watch(lyricStateProvider.notifier);
+
+    return Column(
+      children: [
+        const ListTile(
+            title: Text(
+                'Pick a .LRC file matching ONE of the following conditions:')),
+        RadioListTile(
+          value: 0,
+          groupValue: ref
+              .watch(lyricExistsProvider('${state.title} - ${state.artist}'))
+              .when(
+                data: (data) => data ? 0 : -1,
+                error: (_, __) => -1,
+                loading: () => -1,
               ),
-              ListTile(
-                title: Text(
-                  '2. You can tap the floating window to show/hide the title, progress and button.',
-                ),
+          onChanged: null,
+          title: const Text('File name:'),
+          subtitle: Text('${state.title} - ${state.artist}.lrc'),
+        ),
+        RadioListTile(
+          value: 1,
+          groupValue: ref
+              .watch(lyricExistsProvider('${state.artist} - ${state.title}'))
+              .when(
+                data: (data) => data ? 1 : -1,
+                error: (_, __) => -1,
+                loading: () => -1,
               ),
-              ListTile(
-                title: Text(
-                  '3. Find the "Lyric List" tab in the drawer to edit/delete the imported lyrics.',
-                ),
-              ),
-              ListTile(
-                title: Text(
-                  '4. Killing this app will not close the floating window but will stop the lyric update.',
-                ),
-              ),
-            ],
+          title: const Text('File name:'),
+          subtitle: Text('${state.artist} - ${state.title}.lrc'),
+          onChanged: null,
+        ),
+        RadioListTile(
+          value: 2,
+          groupValue: -1,
+          title: Text('Your .lrc file contains:\n'
+              '[ti:${state.title}]\n'
+              '[ar:${state.artist}]'),
+          onChanged: null,
+        ),
+        const SizedBox(height: 8),
+        SizedBox(
+          width: double.maxFinite,
+          child: FloatingActionButton.large(
+            onPressed: () => state.isProcessingFiles
+                ? null
+                : notifier.pickLyrics().then((failedFiles) {
+                    ref.invalidate(allRawLyricsProvider);
+                    if (failedFiles.isNotEmpty) {
+                      showDialog(
+                        context: context,
+                        builder: (_) => FailedImportDialog(failedFiles),
+                      );
+                    }
+                  }),
+            child: state.isProcessingFiles
+                ? const Center(child: CircularProgressIndicator())
+                : const Text('Import'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ReminderStep extends StatelessWidget {
+  const _ReminderStep();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        ListTile(
+          title: Text(
+            '1. You can drag the floating window to anywhere on the screen.',
+          ),
+        ),
+        ListTile(
+          title: Text(
+            '2. You can tap the floating window to show/hide the title, progress and button.',
+          ),
+        ),
+        ListTile(
+          title: Text(
+            '3. Find the "Lyric List" tab in the drawer to edit/delete the imported lyrics.',
+          ),
+        ),
+        ListTile(
+          title: Text(
+            '4. Killing this app will not close the floating window but will stop the lyric update.',
           ),
         ),
       ],
