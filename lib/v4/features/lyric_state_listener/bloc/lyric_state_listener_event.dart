86c32e1c332c6b6b277ce6b44a38b5a30a9a3137
fewling <felixwong875@gmail.com>
2024-09-30 23:22:26 +0800

    fix: :bug: not searching lyrics automatically

--- lib/v4/features/lyric_state_listener/bloc/lyric_state_listener_event.dart
+++ lib/v4/features/lyric_state_listener/bloc/lyric_state_listener_event.dart
@@ -2,7 +2,13 @@ part of 'lyric_state_listener_bloc.dart';
 
 sealed class LyricStateListenerEvent {}
 
-final class LyricStateListenerLoaded extends LyricStateListenerEvent {}
+final class LyricStateListenerLoaded extends LyricStateListenerEvent {
+  LyricStateListenerLoaded({
+    required this.isAutoFetch,
+  });
+
+  final bool isAutoFetch;
+}
 
 final class AutoFetchUpdated extends LyricStateListenerEvent {
   AutoFetchUpdated({
