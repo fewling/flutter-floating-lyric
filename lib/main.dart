9d7a15ed15502b1581853fd9105bb142d93f0179
fewling <felixwong875@gmail.com>
2024-10-01 15:36:27 +0800

    refactor: :recycle: moved permission bloc to global injector, remove riverpod lints

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:floating_lyric/features/global_dependency_injector/global_dependency_injector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:isar/isar.dart';
@@ -6,6 +5,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'configs/routes/app_router.dart';
+import 'features/global_dependency_injector/global_dependency_injector.dart';
 import 'features/overlay_window/overlay_window.dart';
 import 'features/permissions/bloc/permission_bloc.dart';
 import 'features/preference/bloc/preference_bloc.dart';
@@ -22,11 +22,18 @@ Future<void> main() async {
   );
   final pref = await SharedPreferences.getInstance();
 
+  final permissionBloc = PermissionBloc(
+    permissionService: PermissionService(),
+  )..add(const PermissionEventInitial());
+
+  final router = AppRouter(permissionBloc: permissionBloc);
+
   runApp(
     GlobalDependencyInjector(
       isar: isar,
       pref: pref,
-      child: const FloatingLyricApp(),
+      permissionBloc: permissionBloc,
+      child: FloatingLyricApp(appRouter: router),
     ),
   );
 }
@@ -43,54 +50,31 @@ void overlayMain() {
   );
 }
 
-class FloatingLyricApp extends StatefulWidget {
-  const FloatingLyricApp({super.key});
+class FloatingLyricApp extends StatelessWidget {
+  const FloatingLyricApp({
+    super.key,
+    required this.appRouter,
+  });
 
-  @override
-  State<FloatingLyricApp> createState() => _FloatingLyricAppState();
-}
-
-class _FloatingLyricAppState extends State<FloatingLyricApp> {
-  late final PermissionBloc _permissionBloc;
-  late final AppRouter _appRouter;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _permissionBloc = PermissionBloc(
-      permissionService: PermissionService(),
-    )..add(const PermissionEventInitial());
-
-    _appRouter = AppRouter(
-      permissionBloc: _permissionBloc,
-    );
-  }
+  final AppRouter appRouter;
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider.value(value: _permissionBloc),
-      ],
-      child: Builder(builder: (context) {
-        final isLight = context.select<PreferenceBloc, bool>(
-          (bloc) => bloc.state.isLight,
-        );
+    final isLight = context.select<PreferenceBloc, bool>(
+      (bloc) => bloc.state.isLight,
+    );
 
-        final colorSchemeSeed = context.select<PreferenceBloc, int>(
-          (bloc) => bloc.state.appColorScheme,
-        );
+    final colorSchemeSeed = context.select<PreferenceBloc, int>(
+      (bloc) => bloc.state.appColorScheme,
+    );
 
-        return MaterialApp.router(
-          theme: ThemeData(
-            useMaterial3: true,
-            colorSchemeSeed: Color(colorSchemeSeed),
-            brightness: isLight ? Brightness.light : Brightness.dark,
-          ),
-          routerConfig: _appRouter.router,
-        );
-      }),
+    return MaterialApp.router(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Color(colorSchemeSeed),
+        brightness: isLight ? Brightness.light : Brightness.dark,
+      ),
+      routerConfig: appRouter.router,
     );
   }
 }
