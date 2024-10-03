2f8f06864616e212c52546f296ea7719fd326e71
fewling <felixwong875@gmail.com>
2024-10-04 00:44:34 +0800

    feat: :lipstick: apply themes to overlay window

--- lib/features/overlay_app/overlay_app.dart
+++ lib/features/overlay_app/overlay_app.dart
@@ -43,21 +43,29 @@ class _OverlayAppState extends State<OverlayApp> {
             (MessageFromMainReceiverBloc bloc) => bloc.state.settings,
           );
 
-          return Material(
-            color: Colors.transparent,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context)
-                    .colorScheme
-                    .primaryContainer
-                    .withOpacity((windowSettings?.opacity?.toInt() ?? 50) / 100),
-                borderRadius: BorderRadius.circular(10),
-              ),
+          return Theme(
+            data: ThemeData(
+              colorSchemeSeed: windowSettings?.appColorScheme == null ? null : Color(windowSettings!.appColorScheme),
+              brightness: windowSettings?.isLight ?? true ? Brightness.light : Brightness.dark,
+            ),
+            child: Material(
+              color: Colors.transparent,
               child: Builder(
-                builder: (context) => SizedBox(
-                  width: windowSettings?.width ?? 200,
-                  height: double.infinity,
-                  child: windowSettings == null ? const LoadingWidget() : OverlayWindow(settings: windowSettings),
+                builder: (context) => Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .primaryContainer
+                        .withOpacity((windowSettings?.opacity?.toInt() ?? 50) / 100),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Builder(
+                    builder: (context) => SizedBox(
+                      width: windowSettings?.width ?? 200,
+                      height: double.infinity,
+                      child: windowSettings == null ? const LoadingWidget() : OverlayWindow(settings: windowSettings),
+                    ),
+                  ),
                 ),
               ),
             ),
