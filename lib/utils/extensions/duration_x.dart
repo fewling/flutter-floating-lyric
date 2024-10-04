92b3cdf3a614f8901a75aa1c32f4ab34cc4081f6
fewling <felixwong875@gmail.com>
2024-10-05 05:16:05 +0800

    fix: :bug: online lyric content section not showing current media state

--- lib/utils/extensions/duration_x.dart
+++ lib/utils/extensions/duration_x.dart
@@ -2,7 +2,7 @@ part of 'custom_extensions.dart';
 
 extension DurationX on Duration {
   String mmss() {
-    final minutes = inMinutes;
+    final minutes = inMinutes.toString().padLeft(2, '0');
     final seconds = inSeconds.remainder(60).toString().padLeft(2, '0');
     return '$minutes:$seconds';
   }
