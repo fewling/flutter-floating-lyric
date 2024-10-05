86a7dc81479402be12d393ba56062ec1ce32acc9
fewling <felixwong875@gmail.com>
2024-10-05 16:50:52 +0800

    feat: :lipstick: minor UI update for local import section

--- lib/features/import_local_lrc/import_local_lrc.dart
+++ lib/features/import_local_lrc/import_local_lrc.dart
@@ -12,97 +12,98 @@ class ImportLocalLrc extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    final txtTheme = Theme.of(context).textTheme;
-
     return BlocProvider(
       create: (context) => ImportLocalLrcBloc(
         lrcProcessorService: LrcProcessorService(
           localDB: context.read<LocalDbRepo>(),
         ),
       )..add(const ImportLocalLrcStarted()),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              titleTextStyle: txtTheme.titleSmall?.copyWith(
-                color: colorScheme.onPrimaryContainer,
-              ),
-              title: const Text(
-                'Your LRC file should match one of the following formats:',
-              ),
-            ),
-            Builder(
-              builder: (context) {
-                final title = context.select<LyricStateListenerBloc, String>(
-                  (bloc) => bloc.state.mediaState?.title ?? '',
-                );
-
-                final artist = context.select<LyricStateListenerBloc, String>(
-                  (bloc) => bloc.state.mediaState?.artist ?? '',
-                );
-
-                return Column(
-                  children: [
-                    ListTile(
-                      title: const Text('1. File name should be:'),
-                      subtitle: SelectableText(
-                        '$title - $artist.lrc',
-                        style: TextStyle(color: colorScheme.outline),
-                      ),
-                    ),
-                    ListTile(
-                      title: const Text('2. File name should be:'),
-                      subtitle: SelectableText(
-                        '$artist - $title.lrc',
-                        style: TextStyle(color: colorScheme.outline),
-                      ),
-                    ),
-                    ListTile(
-                      title: const Text('3. File should contain:'),
-                      subtitle: SelectableText(
-                        '[ti:$title]\n'
-                        '[ar:$artist]',
-                        style: TextStyle(color: colorScheme.outline),
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-            Builder(
-              builder: (context) {
-                final isProcessing = context.select<ImportLocalLrcBloc, bool>(
-                  (bloc) => bloc.state.isProcessingFiles,
-                );
-
-                return isProcessing
-                    ? ElevatedButton.icon(
-                        onPressed: null,
-                        icon: const Center(child: CircularProgressIndicator()),
-                        label: const Text('Importing...'),
-                      )
-                    : BlocListener<ImportLocalLrcBloc, ImportLocalLrcState>(
-                        listenWhen: (previous, current) => previous.failedFiles != current.failedFiles,
-                        listener: (context, state) {
-                          if (state.failedFiles.isNotEmpty) {
-                            showDialog(
-                              context: context,
-                              builder: (_) => FailedImportDialog(state.failedFiles),
-                            );
-                          }
-                        },
-                        child: ElevatedButton.icon(
-                          onPressed: () => context.read<ImportLocalLrcBloc>().add(const ImportLRCsRequested()),
-                          icon: const Icon(Icons.drive_folder_upload_outlined),
-                          label: const Text('Import'),
-                        ),
-                      );
-              },
-            ),
-          ],
+      child: const Scaffold(
+        body: SingleChildScrollView(
+          child: _LrcFormatInstruction(),
         ),
+        floatingActionButton: _ImportFab(),
+      ),
+    );
+  }
+}
+
+class _LrcFormatInstruction extends StatelessWidget {
+  const _LrcFormatInstruction();
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final txtTheme = Theme.of(context).textTheme;
+
+    final title = context.select<LyricStateListenerBloc, String>(
+      (bloc) => bloc.state.mediaState?.title ?? '',
+    );
+
+    final artist = context.select<LyricStateListenerBloc, String>(
+      (bloc) => bloc.state.mediaState?.artist ?? '',
+    );
+
+    return Column(
+      children: [
+        ListTile(
+          titleTextStyle: txtTheme.titleSmall?.copyWith(
+            color: colorScheme.onPrimaryContainer,
+          ),
+          title: const Text(
+            'Your LRC file should match one of the following formats:',
+          ),
+        ),
+        ListTile(
+          title: const Text('1. File name should be:'),
+          subtitle: SelectableText(
+            '$title - $artist.lrc',
+            style: TextStyle(color: colorScheme.outline),
+          ),
+        ),
+        ListTile(
+          title: const Text('2. File name should be:'),
+          subtitle: SelectableText(
+            '$artist - $title.lrc',
+            style: TextStyle(color: colorScheme.outline),
+          ),
+        ),
+        ListTile(
+          title: const Text('3. File should contain:'),
+          subtitle: SelectableText(
+            '[ti:$title]\n'
+            '[ar:$artist]',
+            style: TextStyle(color: colorScheme.outline),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ImportFab extends StatelessWidget {
+  const _ImportFab();
+
+  @override
+  Widget build(BuildContext context) {
+    final isProcessing = context.select<ImportLocalLrcBloc, bool>(
+      (bloc) => bloc.state.isProcessingFiles,
+    );
+
+    return BlocListener<ImportLocalLrcBloc, ImportLocalLrcState>(
+      listenWhen: (previous, current) => previous.failedFiles != current.failedFiles,
+      listener: (context, state) {
+        if (state.failedFiles.isNotEmpty) {
+          showDialog(
+            context: context,
+            builder: (_) => FailedImportDialog(state.failedFiles),
+          );
+        }
+      },
+      child: FloatingActionButton.extended(
+        onPressed: isProcessing ? null : () => context.read<ImportLocalLrcBloc>().add(const ImportLRCsRequested()),
+        label: Text(isProcessing ? 'Importing...' : 'Import'),
+        icon: isProcessing ? const CircularProgressIndicator() : const Icon(Icons.drive_folder_upload_outlined),
       ),
     );
   }
