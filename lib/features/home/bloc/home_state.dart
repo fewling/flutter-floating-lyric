3141a07f96ef6f55b3c875c8b80d2acca1afabf5
fewling <felixwong875@gmail.com>
2024-10-05 16:44:24 +0800

    refactor: :coffin: remove unused code

--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -4,10 +4,6 @@ part of 'home_bloc.dart';
 class HomeState with _$HomeState {
   const factory HomeState({
     @Default(null) MediaState? mediaState,
-    @Default(false) bool isWindowVisible,
-    @Default(false) bool isWindowLocked,
-    @Default(false) bool isWindowTouchThrough,
-    @Default(false) bool isWIndowIgnoreTouch,
     @Default(false) bool isSearchingOnline,
     @Default(null) String? titleAlt,
     @Default(null) String? artistAlt,
