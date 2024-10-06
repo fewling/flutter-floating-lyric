426a0c21cb1746243d7e788ee68b1fbc70ddad07
fewling <felixwong875@gmail.com>
2024-10-06 17:04:33 +0800

    feat: :lipstick: change drawer to btm nav bar

--- lib/features/base/base_drawer_routes.dart
+++ lib/features/base/base_drawer_routes.dart
@@ -5,25 +5,30 @@ enum BaseDrawerRoutes {
     route: AppRoute.home,
     label: 'Home',
     icon: Icons.home_outlined,
+    selectedIcon: Icons.home,
   ),
   localLyrics(
     route: AppRoute.localLyrics,
     label: 'Stored Lyrics',
     icon: Icons.audio_file_outlined,
+    selectedIcon: Icons.audio_file,
   ),
   settings(
     route: AppRoute.settings,
     label: 'Settings',
     icon: Icons.settings_outlined,
+    selectedIcon: Icons.settings,
   );
 
   const BaseDrawerRoutes({
     required this.route,
     required this.label,
     required this.icon,
+    required this.selectedIcon,
   });
 
   final AppRoute route;
   final String label;
   final IconData icon;
+  final IconData selectedIcon;
 }
