a7e82807edf8560cb43116a9243b31ede0263908
fewling <felixwong875@gmail.com>
2024-10-31 00:16:18 +0800

    fix: :bug: fix bug report button not triggering intent

--- lib/features/settings/bloc/settings_bloc.dart
+++ lib/features/settings/bloc/settings_bloc.dart
@@ -1,5 +1,7 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 part 'settings_bloc.freezed.dart';
@@ -15,18 +17,59 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     );
   }
 
-  void _onFeedbackEmailClicked(FeedbackEmailClicked event, Emitter<SettingsState> emit) {
+  Future<void> _onFeedbackEmailClicked(FeedbackEmailClicked event, Emitter<SettingsState> emit) async {
+    final packageInfo = await PackageInfo.fromPlatform();
+
+    final version = packageInfo.version;
+    final buildNumber = packageInfo.buildNumber;
+
+    final deviceInfoPlugin = DeviceInfoPlugin();
+    final androidInfo = await deviceInfoPlugin.androidInfo;
+
+    final body = '''
+# FeedBack/Feature Request:
+Please provide information about the feedback or feature request you would like to share here... (if any)
+
+# Bug Report
+Please ignore this section, if you are not reporting bugs.
+      
+## Steps to Reproduce
+1. Step 1
+2. Step 2
+3. Step 3
+
+## Expected Behavior
+What should have happened?
+
+## Actual Behavior
+What happened instead?
+
+## Environment
+The following information will help us to diagnose the issue, feel free to remove any information you don't want to share:
+
+App Version: $version ($buildNumber)
+Manufacturer: ${androidInfo.manufacturer}
+Brand: ${androidInfo.brand}
+Device: ${androidInfo.device}
+Model: ${androidInfo.model}
+Product: ${androidInfo.product}
+Version: ${androidInfo.version.sdkInt}
+Hardware: ${androidInfo.hardware}
+Display: ${androidInfo.display}
+Is Low Ram Device: ${androidInfo.isLowRamDevice}
+Is Physical Device: ${androidInfo.isPhysicalDevice}
+Tags: ${androidInfo.tags}
+''';
+
     final uri = Uri(
       scheme: 'mailto',
       path: 'floatinglyricsapp@gmail.com',
 
       // TODO(@fewling): Include the followings:
-      // - version number
-      // - build number
-      // - device model
-      // - OS version
-      // Take reference to Flutter's issue template.
-      query: _encodeQueryParameters({'subject': 'Feedback'}),
+      query: _encodeQueryParameters({
+        'subject': 'Feedback',
+        'body': body,
+      }),
     );
     launchUrl(uri);
   }
