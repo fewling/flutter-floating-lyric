34d754b2fc05291acc74f47eb78ffaeb5b990418
fewling <felixwong875@gmail.com>
2025-05-22 22:35:05 +0800

    feat: :sparkles: integrate Firebase and FlutterFire configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:isar/isar.dart';
@@ -9,6 +10,7 @@ import 'features/global_dependency_injector/global_dependency_injector.dart';
 import 'features/overlay_app/overlay_app.dart';
 import 'features/permissions/bloc/permission_bloc.dart';
 import 'features/preference/bloc/preference_bloc.dart';
+import 'firebase_options.dart';
 import 'models/lyric_model.dart';
 import 'service/permissions/permission_service.dart';
 import 'service/platform_methods/permission_channel_service.dart';
@@ -16,11 +18,10 @@ import 'service/platform_methods/permission_channel_service.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   final dir = await getApplicationDocumentsDirectory();
-  final isar = await Isar.open(
-    [LrcDBSchema],
-    directory: dir.path,
-  );
+  final isar = await Isar.open([LrcDBSchema], directory: dir.path);
   final pref = await SharedPreferences.getInstance();
 
   final permissionBloc = PermissionBloc(
@@ -43,23 +44,28 @@ Future<void> main() async {
 @pragma('vm:entry-point')
 void overlayView() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(LayoutBuilder(builder: (context, constraints) {
-    return const OverlayApp();
-  }));
+  runApp(
+    LayoutBuilder(
+      builder: (context, constraints) {
+        return const OverlayApp();
+      },
+    ),
+  );
 }
 
 class FloatingLyricApp extends StatelessWidget {
-  const FloatingLyricApp({
-    super.key,
-    required this.appRouter,
-  });
+  const FloatingLyricApp({super.key, required this.appRouter});
 
   final AppRouter appRouter;
 
   @override
   Widget build(BuildContext context) {
-    final isLight = context.select<PreferenceBloc, bool>((bloc) => bloc.state.isLight);
-    final colorSchemeSeed = context.select<PreferenceBloc, int>((bloc) => bloc.state.appColorScheme);
+    final isLight = context.select<PreferenceBloc, bool>(
+      (bloc) => bloc.state.isLight,
+    );
+    final colorSchemeSeed = context.select<PreferenceBloc, int>(
+      (bloc) => bloc.state.appColorScheme,
+    );
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
